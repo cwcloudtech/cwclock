a8d7cb5a1f3edec9b46f3d7660661cca48c69646
Idriss Neumann <idriss.neumann@comwork.io>
2026-07-06 14:58:30 +0100

    fix(cicd): bad CWCLOCK_APPS environment variable

--- ci/compute-env.sh
+++ ci/compute-env.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-export CWCLOUD_APPS="ui api"
+export CWCLOCK_APPS="ui api"
 export VERSION="${EDITION}-$(grep -oE "^[0-9\.]+$" VERSION)"
 export VERSION_SHA="${VERSION}-${CI_COMMIT_SHORT_SHA}"
