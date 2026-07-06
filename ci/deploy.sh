e28e95065a875555c3701a3c31cd5e96895e8fe5
Idriss Neumann <idriss.neumann@comwork.io>
2026-07-06 19:16:20 +0100

    feat(ux-ui): fix env file names

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,13 +2,13 @@
 
 source ./ci/compute-env.sh
 
-echo "" > .env.cwcloud.db
+echo "" > .env.cwclock.db
 env|grep "POSTGRES_"|while read; do
-  echo "${REPLY}" >> .env.cwcloud.db
+  echo "${REPLY}" >> .env.cwclock.db
 done
 
-echo "" > .env.cwcloud.api
-echo "API_URL=${API_URL}" > .env.cwcloud.ui
+echo "" > .env.cwclock.api
+echo "API_URL=${API_URL}" > .env.cwclock.ui
 
 docker ps -a | grep -i cwclock | awk '{system ("docker rm -f "$1)}'
 docker compose -f docker-compose-live.yml up -d --force-recreate
