3a7927a8c4265ed92c440ac3185bf92d512f4f54
Idriss Neumann <idriss.neumann@comwork.io>
2026-07-06 19:18:34 +0100

    feat(cicd): print the db migration logs

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -10,5 +10,6 @@ done
 echo "" > .env.cwclock.api
 echo "API_URL=${API_URL}" > .env.cwclock.ui
 
-docker ps -a | grep -i cwclock | awk '{system ("docker rm -f "$1)}'
+docker ps -a | grep -i cwclock | awk '{system ("docker rm -f "$1)}' || :
 docker compose -f docker-compose-live.yml up -d --force-recreate
+docker logs cwclock-db-migrate || :
