a4c6609880dbd82b3946a8bbc4a1967f78a9b3e0
Idriss Neumann <idriss.neumann@comwork.io>
2026-07-09 11:26:38 +0100

    fix(react-warnings): fix the linter warnings

--- cwclock-ui/src/Components/TimerApp/useTimer.jsx
+++ cwclock-ui/src/Components/TimerApp/useTimer.jsx
@@ -1,4 +1,4 @@
-import {useState,useEffect} from 'react';
+import {useState,useEffect,useRef} from 'react';
 
 const useTimer = () => {
 const [sec,setSec]=useState(0);
@@ -6,35 +6,35 @@ const [min,setMin]=useState(0);
 const [hrs,setHrs]=useState(0);
 const [timerOn,setTimerOn]=useState(false);
 
-var timer;
+const timerRef=useRef(null);
 const handleTimer=()=>{
    if(timerOn){
     setTimerOn(false);
     setHrs(0);
     setMin(0);
     setSec(0);
-     return clearInterval(timer);
+     return clearInterval(timerRef.current);
    }
    setTimerOn(true);
 }
 useEffect(()=>{
     if(timerOn){
-    timer=setInterval(() => {
-    if(min==59 && sec==59){
+    timerRef.current=setInterval(() => {
+    if(min===59 && sec===59){
         setMin(0);
         setSec(0);
-        setHrs(hrs+1);   
+        setHrs(hrs+1);
     }
-    else if(sec==59){
+    else if(sec===59){
         setMin(min+1);
         setSec(0);
-        
+
     }else{
         setSec(sec+1);
     }
     },1000);
 }
-    return ()=>clearInterval(timer);
+    return ()=>clearInterval(timerRef.current);
 })
   return {sec,min,hrs,timerOn,handleTimer}
 }
